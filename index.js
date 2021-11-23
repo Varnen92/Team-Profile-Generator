d554e7aa50fea61bc7cdc7ed7c83a25ff0a33ce3
Robert Sandoval <robsand1992@yahoo.com>
2021-11-23 12:35:55 -0800

    added create Manager prompts and pushed manager to array

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+var teamMembers = [] 
 
- 
-let createTeam = () => {
-    inquirer
-        .prompt({
-            type: 'list',
-            name: 'role',
-            message: 'Please choose a role to add to your Team!',
-            choices: ["Manager", "Engineer", "Intern", "No more members!"]
-        })
-        .then(({ role }) => {
-            switch (role) {
-                case "Manager":
-                    createManager()
-                    break;
-                case "Engineer":
-                    createEngineer()
-                    break;
-                case "Intern":
-                    createIntern()
-                    break;
-                case "No more members!":
-                    // add fswrite later
-                    break
-            }
-        })
-}
+
 
 let createManager = () => {
     inquirer
@@ -48,8 +27,47 @@ let createManager = () => {
                 name: 'email',
                 message: 'Please enter the team member email:',
 
-            }])
-        .then(res => console.log(res))
+            },
+            {
+                type:'text',
+                name: 'office',
+                message: 'Please enter your office number:'
+            }
+        
+        ])
+        .then(data => {
+            const manager = new Manager(data.name, data.id, data.email, data.office)
+            teamMembers.push(manager)
+            console.log(teamMembers)
+            teamPrompt()})
 }
-createTeam() 
+
+let createEngineer = () => {
+    inquirer
+    .prompt([])
+}
+
+let teamPrompt = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'role',
+            message: 'Please choose a role to add to your Team!',
+            choices: ["Engineer", "Intern", "No more members!"]
+        })
+        .then(({ role }) => {
+            switch (role) {
+                case "Engineer":
+                    createEngineer()
+                    break;
+                case "Intern":
+                    createIntern()
+                    break;
+                case "No more members!":
+                    // add fswrite later
+                    break
+            }
+        })
+}
+createManager() 
 
