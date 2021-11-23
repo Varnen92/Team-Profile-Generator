cf7b22d3c2703b3f53b8765655867a08f0a09bf5
Robert Sandoval <robsand1992@yahoo.com>
2021-11-22 19:01:59 -0800

    Modified index to move into create Manager and create roles

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
+const inquirer = require('inquirer')
 const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
 
 
-new Employee().getName()
+let createTeam = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'role',
+            message: 'Please choose a role to add to your Team!',
+            choices: ["Manager", "Engineer", "Intern", "No more members!"]
+        })
+        .then(({ role }) => {
+            switch (role) {
+                case "Manager":
+                    createManager()
+                    break;
+                case "Engineer":
+                    createEngineer()
+                    break;
+                case "Intern":
+                    createIntern()
+                    break;
+                case "No more members!":
+                    // add fswrite later
+                    break
+            }
+        })
+}
+
+let createManager = () => {
+    inquirer
+    .prompt([{
+        type: 'text',
+        name: 'id',
+        message: 'Please enter the ID Number of the team member:', 
+    },
+    {
+        type: 'text',
+        name: 'email',
+        message: 'Please enter the team member email:',
+
+    }])
+    .then(res => console.log(res))
+}
+    createManager()
